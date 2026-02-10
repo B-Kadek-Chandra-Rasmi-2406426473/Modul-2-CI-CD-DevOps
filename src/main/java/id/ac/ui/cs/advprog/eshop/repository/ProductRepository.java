6384ea621c7290a7804eff2bfcb987097e9e2378
kyraa16 <kadekchandra16@gmail.com>
2026-02-10 10:08:31 +0700

    feat: delete product

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -6,14 +6,22 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
     public Product create(Product product) {
+        if (product.getProductId() == null || product.getProductId().isEmpty()) {
+            product.setProductId(UUID.randomUUID().toString());
+        }
         productData.add(product);
         return product;
+    }
+
+    public boolean delete(String id) {
+        return productData.removeIf(product -> product.getProductId().equals(id));
     }
 
     public Iterator<Product> findAll() {
