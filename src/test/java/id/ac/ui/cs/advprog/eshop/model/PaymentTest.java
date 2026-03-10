203ebf2ef7c6ff3eb8908a6214fb0a12351e486b
kyraa16 <kadekchandra16@gmail.com>
2026-03-10 12:28:19 +0700

    [RED] Add Payment  model skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -1,8 +1,5 @@
-package model;
+package id.ac.ui.cs.advprog.eshop.model;
 
-import id.ac.ui.cs.advprog.eshop.model.Order;
-import id.ac.ui.cs.advprog.eshop.model.Payment;
-import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
