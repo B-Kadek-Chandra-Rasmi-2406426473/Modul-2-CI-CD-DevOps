c1cf09ea50ab3255528721941f9ac21e431788de
kyraa16 <kadekchandra16@gmail.com>
2026-03-10 17:23:50 +0700

    [RED] functional test for OrderController

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/OrderControllerTest.java
@@ -0,0 +1,93 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.service.OrderService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = OrderController.class)
+class OrderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private OrderService orderService;
+
+    private Order order;
+
+    @BeforeEach
+    void setUp() {
+        List<Product> products = new ArrayList<>();
+        Product product = new Product();
+        product.setProductId("prod-1");
+        product.setProductName("Test Product");
+        product.setProductQuantity(1);
+        products.add(product);
+
+        order = new Order("order-123", products, 1708560000L, "Chandra");
+    }
+
+    @Test
+    void testCreateOrderPage() throws Exception {
+        mockMvc.perform(get("/order/create"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("CreateOrder"));
+    }
+
+    @Test
+    void testCreateOrderPost() throws Exception {
+        Mockito.when(orderService.createOrder(any(Order.class))).thenReturn(order);
+
+        mockMvc.perform(post("/order/create")
+                        .param("author", "Chandra"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/order/history"));
+    }
+
+    @Test
+    void testHistoryOrderPage() throws Exception {
+        mockMvc.perform(get("/order/history"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("OrderHistory"));
+    }
+
+    @Test
+    void testHistoryOrderPost() throws Exception {
+        List<Order> orders = new ArrayList<>();
+        orders.add(order);
+        Mockito.when(orderService.findAllByAuthor("Chandra")).thenReturn(orders);
+
+        mockMvc.perform(post("/order/history")
+                        .param("author", "Chandra"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("OrderList"))
+                .andExpect(model().attributeExists("orders"))
+                .andExpect(model().attributeExists("author"));
+    }
+
+    @Test
+    void testPayOrderPage() throws Exception {
+        Mockito.when(orderService.findById("order-123")).thenReturn(order);
+
+        mockMvc.perform(get("/order/pay/order-123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("PayOrder"))
+                .andExpect(model().attributeExists("order"));
+    }
+}
