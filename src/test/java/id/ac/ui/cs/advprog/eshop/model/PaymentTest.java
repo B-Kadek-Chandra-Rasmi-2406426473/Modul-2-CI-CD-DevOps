fa0ff02ebc38628d92cde7623122526863ca1803
kyraa16 <kadekchandra16@gmail.com>
2026-03-10 12:26:18 +0700

    [RED] Add tests for Payment model

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -0,0 +1,75 @@
+package model;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentTest {
+    private List<Product> products;
+    private Order order;
+
+    @BeforeEach
+    void setUp() {
+        this.products = new ArrayList<>();
+        Product product1 = new Product();
+        product1.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product1.setProductName("Sampo Cap Bambang");
+        product1.setProductQuantity(2);
+        this.products.add(product1);
+
+        this.order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+                this.products, 1708560000L, "Safira Sudrajat");
+    }
+
+    @Test
+    void testCreatePaymentVoucherSuccess() {
+        Map<String, String> paymentData = new HashMap<>();
+        paymentData.put("voucherCode", "ESHOP1234ABC5678");
+        Payment payment = new Payment("payment-1", order, "VOUCHER", paymentData);
+        assertEquals("SUCCESS", payment.getStatus());
+    }
+
+    @Test
+    void testCreatePaymentVoucherRejected() {
+        Map<String, String> paymentData = new HashMap<>();
+        paymentData.put("voucherCode", "ESHOP1234");
+        Payment payment = new Payment("payment-2", order, "VOUCHER", paymentData);
+        assertEquals("REJECTED", payment.getStatus());
+    }
+
+    @Test
+    void testCreatePaymentCodSuccess() {
+        Map<String, String> paymentData = new HashMap<>();
+        paymentData.put("address", "Jalan Margonda Raya");
+        paymentData.put("deliveryFee", "10000");
+        Payment payment = new Payment("payment-3", order, "CASH_ON_DELIVERY", paymentData);
+        assertEquals("SUCCESS", payment.getStatus());
+    }
+
+    @Test
+    void testCreatePaymentCodRejectedIfAddressEmpty() {
+        Map<String, String> paymentData = new HashMap<>();
+        paymentData.put("address", "");
+        paymentData.put("deliveryFee", "10000");
+        Payment payment = new Payment("payment-4", order, "CASH_ON_DELIVERY", paymentData);
+        assertEquals("REJECTED", payment.getStatus());
+    }
+
+    @Test
+    void testCreatePaymentCodRejectedIfDeliveryFeeEmpty() {
+        Map<String, String> paymentData = new HashMap<>();
+        paymentData.put("address", "Jalan Margonda Raya");
+        paymentData.put("deliveryFee", "");
+        Payment payment = new Payment("payment-5", order, "CASH_ON_DELIVERY", paymentData);
+        assertEquals("REJECTED", payment.getStatus());
+    }
+}
